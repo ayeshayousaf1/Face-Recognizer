655bc883996ef3084a8a8b4a240db41edfec5f05
ayeshayousaf1 <myousaf3038@gmail.com>
2024-10-17 10:58:15 +0000

    face mask recognizer

--- streamlitapp.py
+++ streamlitapp.py
@@ -7,13 +7,6 @@ import numpy as np
 model = tf.keras.models.load_model('model.h5')
 
 
-# Define a function to preprocess the uploaded image
-def preprocess_image(image):
-    image = image.resize((150, 150))
-    image = np.array(image) / 255.0
-    image = np.expand_dims(image, axis=0)
-    return image
-
 # Streamlit app
 st.title("Face Mask Detection")
 
@@ -30,13 +23,16 @@ if uploaded_file is not None:
     image = image.convert("RGB")
     image = image.resize((128, 128))
     
-
     # Preprocess the image
-    processed_image = preprocess_image(image)
-
-    # Predict the mask status
-    prediction = model.predict(processed_image)
-
+    img_array = np.array(image)
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    img_array = img_array / 255.0  # Normalize
+    
+    # Predict using the model
+    prediction = model.predict(img_array)
+    class_idx = np.argmax(prediction)
+    
+    
     # Display the prediction
     if prediction[0] > 0.5:
         st.write("The person is NOT wearing a mask.")
