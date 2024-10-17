0412acd0db5404224a0051cbfcb78cdf3f82c16b
ayeshayousaf1 <myousaf3038@gmail.com>
2024-10-17 10:42:17 +0000

    Face Mask Recognizer

--- streamlitapp.py
+++ streamlitapp.py
@@ -26,6 +26,11 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
+    # Convert the image to RGB and resize it to 128x128
+    image = image.convert("RGB")
+    image = image.resize((128, 128))
+    
+
     # Preprocess the image
     processed_image = preprocess_image(image)
 
